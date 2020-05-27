2d823ef19892664ceb6a6efc8494963fcdc0fd43
Alvin Alaphat <alvinalaphat@gmail.com>
2020-05-26 21:21:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route("/alvinalaphat.git", methods=['GET','POST'])
 def hello():
     return render_template("dashboard.html")
+
+@app.route("/")
+def dash():
+    return render_template("dashboard.html")
