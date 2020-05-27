21e66effe17b30148ea7ae6d125210c17b615b57
Alvin Alaphat <alvinalaphat@gmail.com>
2020-05-26 21:20:42 -0500

    adding routes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/alvinalaphat.git")
+@app.route("/alvinalaphat.git", methods=['GET','POST'])
 def hello():
-    return "Welcome to Alvin's Personal Website!"
+    return render_template("dashboard.html")
